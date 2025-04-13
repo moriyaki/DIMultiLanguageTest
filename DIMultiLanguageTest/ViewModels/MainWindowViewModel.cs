4636a97cc215349665b84dcde5b5095e961adbbb
moriyaki <moriyaki999@gmail.com>
2025-04-13 13:58:01 +0900

    .NET 9.0 Release

---
+++ DIMultiLanguageTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DIMultiLanguageTest.Services;
+
+namespace DIMultiLanguageTest.ViewModels
+{
+    public interface IMainWindowViewModel { }
+
+    public partial class MainWindowViewModel : ObservableObject, IMainWindowViewModel
+    {
+        /// <summary>
+        /// 挨拶をするボタンのテキスト
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression")]
+        public string Greetings
+        { get => ResourceService.GetString("Greetings"); }
+
+        /// <summary>
+        /// 挨拶のためのカルチャーコードを変更します。
+        /// </summary>
+        /// <param name="cultureCode">カルチャーコード</param>
+        private void ChangeGreetingsCulture(string cultureCode)
+        {
+            ResourceService.ChangeCulture(cultureCode);
+            OnPropertyChanged(nameof(Greetings));
+        }
+
+        [RelayCommand]
+        private void ToEnglish()
+        {
+            ChangeGreetingsCulture("en");
+        }
+
+        [RelayCommand]
+        private void ToJapanese()
+        {
+            ChangeGreetingsCulture("ja");
+        }
+
+        [RelayCommand]
+        private void ToRussian()
+        {
+            ChangeGreetingsCulture("ru");
+        }
+    }
+}
