06ad73f4270a3121475c5e61c34aaec916872b4b
moriyaki <moriyaki999@gmail.com>
2025-06-17 01:47:19 +0900

    NuGet Update

--- DIMultiLanguageTest/obj/Debug/net9.0-windows/App.g.cs
+++ DIMultiLanguageTest/obj/Debug/net9.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace DIMultiLanguageTest {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
         public void InitializeComponent() {
             
             #line 6 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace DIMultiLanguageTest {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.6.0")]
         public static void Main() {
             DIMultiLanguageTest.App app = new DIMultiLanguageTest.App();
             app.InitializeComponent();
--- DIMultiLanguageTest/obj/Debug/net9.0-windows/Views/MainWindow.g.i.cs
+++ DIMultiLanguageTest/obj/Debug/net9.0-windows/Views/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace DIMultiLanguageTest {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DIMultiLanguageTest;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DIMultiLanguageTest;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
